6cc204edd091e2ac2a8073c83c9642bc98094994
Rublov Viacheslav <timmy31101994@gmail.com>
2022-07-29 21:36:41 +0300

    HW_M5L1

---
+++ Module5HW1/Models/Resource.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Module5HW1.Models
+{
+    public class Resource
+    {
+        public int Id { get; init; }
+        public string Name { get; init; }
+        public int Year { get; init; }
+        public string Color { get; init; }
+        public string Pantone_Value { get; init; }
+    }
+}
